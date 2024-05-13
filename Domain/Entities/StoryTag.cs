28d7f07e14a3da6396d8e24ba6407b0940205ee2
Võ Tấn Phát <recca5p@gmail.com>
2024-05-13 12:20:54 +0700

    reinitial

--- Domain/Entities/StoryTag.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class StoryTag
-{
-    public Int64 StoryId { get; set; }
-    public Int64 TagId { get; set; }
-    public virtual Story Story { get; set; }
-    public virtual Tag Tag { get; set; }
-}
--- Domain/Entities/Tag.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Entities;
-
-public class Tag : DeleteEntity<Int64>
-{
-    public String Name { get; set; }
-
-    public virtual ICollection<StoryTag> StoryTags { get; set; } = new HashSet<StoryTag>();
-}
